e81b7142754c1ca5d1c1b5cfe06346c5b84d4b7d
AFSAR ALAM <167625167+AfsarAlam123@users.noreply.github.com>
2024-04-24 00:57:22 +0530

    Add files via upload

---
+++ a.java
@@ -0,0 +1,25 @@
+
+public class a {
+	public static void main(String[] args) {
+		byte age = 24;
+		short yop = 2023;
+		int salery = 68344;
+		long phone = 9523519269l;
+		float precentage  = 8.5f;
+		double marks = 67.4; 
+		char grade = 'A';
+		boolean pass = true;
+		System.err.println("print the user of age          = "+age);
+		System.err.println("print the user of yop          = "+yop);
+		System.err.println("print the user of salery       = "+salery);
+		System.err.println("print the user of phone number = "+phone  );
+		System.err.println("print the user of precentage   = "+precentage);
+		System.err.println("print the user of marks        = "+marks);
+		System.err.println("print the user of grade        = "+grade);
+		System.err.println("print the user of pass         = "+true);
+		
+		
+		
+	}
+
+}
